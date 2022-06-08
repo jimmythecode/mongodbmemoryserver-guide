7fde9de360f3d1e3c2583ba8246687b2e8f881e3
jimmythecode <jimmythecode@gmail.com>
2022-06-08 17:03:27 +0100

    can't get guide example working

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
+  preset: 'ts-jest',
   clearMocks: true,
   roots: ['<rootDir>/src'],
   testEnvironment: 'node',
-  preset: 'ts-jest'
+  // Mongodb-memory-server: https://nodkz.github.io/mongodb-memory-server/docs/guides/integration-examples/test-runners :
+  globalSetup: '<rootDir>/test/globalSetup.ts',
+  globalTeardown: '<rootDir>/test/globalTeardown.ts',
+  setupFilesAfterEnv: ['<rootDir>/test/setupFile.ts'],
 };
